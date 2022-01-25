1030bbfe1190d8abc995c6214feda514d280ea1e
Paul Z <pzpzpzp1@gmail.com>
2022-01-25 05:19:17 -0500

    new scene

--- mtlbsrc/isosurfviz.m
+++ mtlbsrc/isosurfviz.m
@@ -16,8 +16,9 @@ hold all; axis equal; rotate3d on;  view(3);
 xlim([fnewBBLL(1) fnewBBTR(1)]);
 ylim([fnewBBLL(2) fnewBBTR(2)]);
 zlim([fnewBBLL(3) fnewBBTR(3)]);
-campos([0            0       12.657])
-% campos([-12.655      0.20839    -0.045088])
+campos([2.5316    -0.050836       12.401]) % face side view
+campos([3.26       3.3893       11.751]) % face side view
+% campos([12.056     -0.60772       3.8065]) % thumb side view
 camup([0 1 0])
 camtarget([0 0 0])
 for i=1:nf
@@ -48,7 +49,7 @@ for i=1:nf
 %     prcs = [30:5:30]/5*2;
     prcs = [40:5:50]/5*2;
     prcs = [.05:.01:.06];
-    prcs = [.04:.01:.05];
+    prcs = [.04:.02:.06];
     for prci = 1:numel(prcs)
         prc = prcs(prci)
 %         [FF,VV] = isosurface(x,y,z,gridsdf,prctile(gridsdf(:),prc));
@@ -56,9 +57,13 @@ for i=1:nf
         fa = .3; if prci == 1; fa = .6; end
     %     fa = .2
         [U,Uall] = laplacian_smooth(double(VV),FF,'cotan',[],.1,'implicit',double(VV),10);
-        ptc{prci} = patch('vertices',Uall(:,:,end),'faces',FF,'facealpha',fa,'edgecolor','none','facecolor','blue')
+        col = [.2 .2 1]
+        ptc{prci} = patch('vertices',Uall(:,:,end),'faces',FF,'facealpha',fa,'edgecolor','none','facecolor',col)
+        ptc{prci}.FaceVertexCData = repmat(col,size(VV,1),1);
+        
     end    
     lighting gouraud; 
+    shading flat
     
     if i==1; camlight; end;
     drawnow; pause(.1)
